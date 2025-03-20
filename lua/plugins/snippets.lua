a1110a392a665b7d3977f31f57a2f0618624eb3a
Hannah Zafar <hannah.zafar@nasa.gov>
2025-03-20 17:19:37 -0400

    Add snippets

--- lua/plugins/snippets.lua
+++ lua/plugins/snippets.lua
@@ -16,7 +16,23 @@ return {
         s("pysh", { t("#!/usr/bin/env python") }),
         s("bashsh", { t("#!/usr/bin/bash") }),
         s("hpcuser", { t("hzafar@discover.nccs.nasa.gov") }),
+        s("modpy", { t("module load python/GEOSpyD/Min24.4.0-0_py3.12") }),
+        --[[
+        s("sbatch", {
+          t('#sbatch --account=s1460'), t('\n'),
+          t('#sbatch --time=0:00:00'), t('\n'),
+          t('#sbatch --constraint="[cas|sky|mil]”'), t('\n'),
+          t('#SBATCH --output=slurm-%j-%a.log'),
+        }) --]]
+        s("sbatch", {
+          t({
+            '#sbatch --account=s1460',
+            '#sbatch --time=0:00:00',
+            '#sbatch --constraint="[cas|sky|mil]”',
+            '#SBATCH --output=slurm-%j-%a.log'
+           }),
       })
+    })
     end,
   }
 }
