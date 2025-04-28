e084241fef10978c7d041fb3a500450c00efbad6
Hannah Zafar <hannah.zafar@nasa.gov>
2025-04-28 18:52:21 -0400

    Reorganize to plugins subfolder

---
+++ lua/plugins/telescope.lua
@@ -0,0 +1,16 @@
+return {
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    config = function()
+      local builtin = require('telescope.builtin')
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope [F]ind [F]iles' })
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope [F]ind by [G]rep' })
+      vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope [F]ind [B]uffers' })
+      vim.keymap.set('n', '<leader>fh', builtin.help_tags , { desc = 'Telescope [F]ind [H]elp tags' })
+      -- Shortcut for searching your Neovim configuration files
+      vim.keymap.set('n', '<leader>fn', function()
+        builtin.find_files { cwd = vim.fn.stdpath 'config' }
+      end, { desc = 'Telescope [F]ind [N]eovim files' })
+    end
+}
+
