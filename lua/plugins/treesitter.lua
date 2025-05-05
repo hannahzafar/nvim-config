d57f930e8a925cac5e2be8cac8627b63992c6514
Hannah Zafar <hannah.zafar@nasa.gov>
2025-05-05 15:46:10 -0400

    Fix treesitter markdown error

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
   config = function()
     local configs = require('nvim-treesitter.configs')
     configs.setup({
-      ensure_installed = { "c", "lua", "vim", "python", "bash" },
+      ensure_installed = { "markdown", "markdown_inline","c", "lua", "vim", "python", "bash" },
       highlight = { enable = true },
       indent = { enable = true },
     })
