4cb8018e57e13392b5b4ee4dc101f9b62c7e6d97
Hannah Zafar <hannahazafar@nasa.gov>
2025-05-05 17:47:03 -0400

    Show inline diagnostics/etc

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -18,6 +18,14 @@ return {
     config = function()
       local lspconfig = require("lspconfig")
       lspconfig.lua_ls.setup({})
+      vim.diagnostic.config({
+        virtual_text = true, -- enable inline errors
+        signs = true,        -- show signs in the gutter
+        underline = true,    -- underline problem text
+        update_in_insert = false, -- don't show diagnostics while typing
+        severity_sort = true,     -- sort by severity
+      })
+
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
       vim.keymap.set({'n','v'}, '<leader>ca', vim.lsp.buf.code_action, {})
