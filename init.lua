81686a7227d06d10bbfaeb1e91c424b9b67312af
Hannah Zafar <hannah.zafar@nasa.gov>
2025-05-13 11:33:16 -0400

    Update order of loading

--- init.lua
+++ init.lua
@@ -2,8 +2,9 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-require("config.lazy")
-
 require("config.options")
 require("config.keymaps")
 require("config.autocmds")
+
+require("config.lazy")
+
