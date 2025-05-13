37dce26de7f3ab10313e0314da299db54251c431
Hannah Zafar <hannah.zafar@nasa.gov>
2025-05-13 17:28:10 -0400

    Set which-key options

--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -2,9 +2,12 @@ return {
   "folke/which-key.nvim",
   event = "VeryLazy",
   opts = {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
+    icons = {
+      mappings = false
+    },
+    spec = {
+      { '<leader>s', group = '[S]earch' },
+    }
   },
   keys = {
     {
