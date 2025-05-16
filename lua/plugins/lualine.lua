7741c427f0085986c3f4432392d02c917aafe179
Hannah Zafar <hannah.zafar@nasa.gov>
2025-05-16 19:30:54 -0400

    Add lsp status to lualine status line

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,10 +1,27 @@
-return{
+return {
   'nvim-lualine/lualine.nvim',
+  dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
-    require('lualine').setup({
-    options = {
-      theme = 'dracula'
-    }
-  })
+    require('lualine').setup{
+      --    options = {
+        --      theme = 'dracula'
+        --    }
+        sections = {
+          lualine_c = {
+            {
+              'filename',
+              path = 1,
+            }
+          },
+          lualine_x = {
+            {
+              'lsp_status',
+              -- List of LSP names to ignore (e.g., `null-ls`):
+              --ignore_lsp = {'null-ls'},
+            },
+            'encoding', 'fileformat', 'filetype'
+          }
+        }
+      }
   end
 }
