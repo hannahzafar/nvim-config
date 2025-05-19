e0ae2e3bc0fd301d7e35425a301e0764eaf44b6d
Hannah Zafar <hannah.zafar@nasa.gov>
2025-05-19 17:35:47 -0400

    Codecompanion chat appearance

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -28,7 +28,18 @@ return {
           })
         end
       }
+    },
+      dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
     }
+  },
+  {
+      'MeanderingProgrammer/render-markdown.nvim',
+      dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
+      ---@module 'render-markdown'
+      ---@type render.md.UserConfig
+      ft = { "markdown", "codecompanion" },
   }
 }
 
