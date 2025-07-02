30d390e1cc7c9107caa77513d69d8b98f9458d88
Hannah Zafar <hannah.zafar@nasa.gov>
2025-07-02 17:21:10 -0400

    Update delete buffer shortcut

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -44,15 +44,21 @@ return {
 
       -- [[ Configure Telescope ]]
       -- See `:help telescope` and `:help telescope.setup()`
+      local actions = require('telescope.actions')
       require('telescope').setup {
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
         --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
+        defaults = {
+          mappings = {
+            i = {
+              ['<C-d>'] = actions.delete_buffer,
+            },
+            n = {
+              ["dd"] = actions.delete_buffer,     -- "dd" in normal mode
+            },
+          },
+        },
         -- pickers = {}
         extensions = {
           ['ui-select'] = {
