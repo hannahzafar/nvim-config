7f22b475c8689e10923e3e7e145098aa1756d4f9
Hannah Zafar <hannah.zafar@nasa.gov>
2025-10-16 17:26:00 -0400

    Gitsign priority over other in column

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -38,7 +38,7 @@ return {
         use_focus = true,
       },
       current_line_blame_formatter = '<author>, <author_time:%R> - <summary>',
-      sign_priority = 6,
+      sign_priority = 100,
       update_debounce = 100,
       status_formatter = nil, -- Use default
       max_file_length = 40000, -- Disable if file is longer than this (in lines)
