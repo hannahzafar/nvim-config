db4d4ffbce40ad605a238a83538c26d6a4bf5072
Hannah Zafar <hannah.zafar@nasa.gov>
2026-03-17 14:56:28 -0400

    Change keymap (already used)

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -2,7 +2,7 @@ return {
   "akinsho/nvim-toggleterm.lua",
   keys = {
     { "<C-\\>",    "<cmd>ToggleTerm<CR>", desc = "Toggle terminal" },
-    { "<leader>t", "<cmd>TermNew<CR>",    desc = "Open new hsplit terminal" },
+    { "<leader>h", "<cmd>TermNew<CR>",    desc = "Open new [h]split terminal" },
   },
   config = function()
     require("toggleterm").setup({
