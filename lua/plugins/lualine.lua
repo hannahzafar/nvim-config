daff5ac2d90976e7e2b77d1d36b5786153070b76
Hannah Zafar <hannah.zafar@nasa.gov>
2026-04-23 16:58:55 -0400

    Modify to toggle a short lualine mode

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -4,8 +4,9 @@ return {
   config = function()
     -- Toggle state: 0 = filename only, 1 = relative path (default)
     local filename_path_mode = 1
+    local lualine_x = {'encoding', 'fileformat', 'filetype'}
 
-    local function require_lualine_setup(PATH_MODE)
+    local function require_lualine_setup(PATH_MODE, LUALINE_X)
       require('lualine').setup{
         options = {
           { globalstatus = true },
@@ -24,38 +25,37 @@ return {
               path = PATH_MODE,
             }
           },
-          lualine_x = {
-            'fileformat', 'filetype'
-          }
+          lualine_x = LUALINE_X,
         }
       }
     end
 
-    -- Function to toggle between filename only and relative path
-    local function toggle_filename_path()
+    -- Function to toggle between full and simple lualine
+    local function toggle_short_lualine()
       if filename_path_mode == 1 then
         filename_path_mode = 0
       else
         filename_path_mode = 1
       end
-      -- Update lualine configuration
-      require_lualine_setup(filename_path_mode)
 
-
-      -- Notify update
       local mode
-      if filename_path_mode == 1 then
-        mode = "ON"
+      if filename_path_mode == 0 then
+        mode = "SHORT"
+        lualine_x = {'filetype'}
       else
-        mode = "OFF"
+        mode = "DEFAULT"
+        lualine_x = {'encoding', 'fileformat', 'filetype'}
       end
-      vim.notify("Toggle path " .. mode .. " in lualine" , vim.log.levels.INFO)
+
+      -- Update lualine configuration and notify
+      require_lualine_setup(filename_path_mode, lualine_x)
+      vim.notify("Lualine " .. mode .. " mode", vim.log.levels.INFO)
     end
 
     -- Create a command and keybinding to toggle
-    vim.api.nvim_create_user_command('ToggleFilenamePath', toggle_filename_path, {})
-    vim.keymap.set('n', '<leader>tp', toggle_filename_path, {
-      desc = '[T]oggle [p]ath in lualine',
+    vim.api.nvim_create_user_command('ToggleLualine', toggle_short_lualine, {})
+    vim.keymap.set('n', '<leader>tl', toggle_short_lualine, {
+      desc = '[T]oggle [l]ualine style',
       noremap = true,
       silent = true,
     })
