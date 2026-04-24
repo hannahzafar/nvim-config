d26e62e15bb29b15f2a16cc5cb3a04285ea5ea7b
Hannah Zafar <hannah.zafar@nasa.gov>
2026-04-24 16:23:59 -0400

    Update searchbox keys (incomplete)

--- lua/plugins/searchbox.lua
+++ lua/plugins/searchbox.lua
@@ -11,7 +11,8 @@ return {
     -- replace
     { "<leader>sbr", mode = {"n"}, function () require("searchbox").replace({confirm = 'menu'}) end, desc = "[S]earch[b]ox [R]eplace" }, -- confirm replace in normal mode
     { "<leader>sbR", mode = {"n"}, function () require("searchbox").replace({confirm = 'menu',default_value = vim.fn.expand("<cword>")}) end, desc = "[S]earch[b]ox replace [C]urrent word" },
-    { "<leader>sbr", mode = {"x"}, function () require("searchbox").replace({visual_mode = true}) end, desc = "[S]earch[b]ox [R]eplace" }, -- replace all in visual mode
+    -- { "<leader>sbr", mode = {"x"}, function () require("searchbox").replace({visual_mode = true, default_value = vim.fn.getreg('v')}) end, desc = "[S]earch[b]ox [R]eplace current selection" }, -- replace current selection # Can't get this working
+    { "<leader>sba", mode = {"x"}, function () require("searchbox").replace({visual_mode = true}) end, desc = "[S]earch[b]ox Replace [A]ll Selected" }, -- replace all in visual mode
   },
 }
 
