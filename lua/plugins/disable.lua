db9c73589824d8e825e2f5b6882b4e7b0ed90134
Hannah Zafar <hannah.zafar@nasa.gov>
2026-04-30 14:21:53 -0400

    Setup to test blackout/save window issues

--- lua/plugins/disable.lua
+++ lua/plugins/disable.lua
@@ -1,5 +1,41 @@
-return {
-  -- disable trouble package for testing
-  -- { "rcarriga/nvim-notify", enabled = false },
+local disable_plugins = {
+  -- "alpha-nvim",
+  -- "autopairs",
+  -- "blink-cmp" ,
+  -- "bmessages",
+  -- "catppuccin",
+  -- "conform" ,
+  -- "copilot",
+  -- "copilotchat",
+  -- "gitsigns",
+  -- "indent_line" ,
+  -- "lsp-config",
+  -- "lualine",
+  -- "neo-tree" ,
+  -- "nvim-surround",
+  -- "render-markdown",
+  -- "searchbox",
+  -- "snacks" ,
+  -- "tabout",
+  -- "telescope",
+  -- "todo-comments" ,
+  -- "toggleterm",
+  -- "treesitter",
+  -- "ufo",
+  -- "which-key",
 }
 
+local function disable_my_plugins(plugin_list)
+  local disabled_plugins = {}
+  for _, plugin in ipairs(plugin_list) do
+    table.insert(disabled_plugins, { plugin, enabled = false })
+  end
+  return disabled_plugins
+end
+
+return {
+  disable_my_plugins(disable_plugins),
+}
+
+
+  -- { "rcarriga/nvim-notify", enabled = false },
