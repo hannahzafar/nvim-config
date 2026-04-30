0d65dbfe1910e7f5d65fecde1b1fad8d0e35a0e8
Hannah Zafar <hannah.zafar@nasa.gov>
2026-04-30 12:40:59 -0400

    Expand sign column for marks

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -37,6 +37,8 @@ vim.o.smartcase = true
 
 -- Keep signcolumn on by default
 vim.o.signcolumn = 'yes'
+-- expand sign column for marks
+vim.opt.signcolumn = "auto:2"
 
 -- Decrease update time
 vim.o.updatetime = 250
