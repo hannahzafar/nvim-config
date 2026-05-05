7f15a1b3757c17a4e2126b2c7c9d706a4e7101c9
Hannah Zafar <hannah.zafar@nasa.gov>
2026-05-05 14:24:53 -0400

    Fix todo keys and auto-load on startup

--- lua/plugins/todo-comments.lua
+++ lua/plugins/todo-comments.lua
@@ -1,6 +1,7 @@
 -- Highlight todo, notes, etc in comments
 return {
   "folke/todo-comments.nvim",
+  lazy = false,
   dependencies = { "nvim-lua/plenary.nvim" },
   opts = {
     -- your configuration comes here
@@ -8,8 +9,8 @@ return {
     -- refer to the configuration section below
   },
   keys  = {
-    { "<leader>oq", ":TodoQuickFix", desc = "T[o]do [q]uickfix list", silent = true},
-    { "<leader>ol", ":TodoLocList", desc =  "T[o]do [l]ocation list", silent = true},
+    { "<leader>oq", ":TodoQuickFix<CR>", desc = "T[o]do [q]uickfix list", silent = true},
+    { "<leader>ol", ":TodoLocList<CR>", desc =  "T[o]do [l]ocation list", silent = true},
     { "<leader>st", ":TodoTelescope<CR>", desc = "[S]earch [t]odo list" }
   }
 }
