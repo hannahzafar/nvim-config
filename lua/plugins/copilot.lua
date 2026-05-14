68d8640669925f09ef96c1b7392a058631d21a2b
Hannah Zafar <hannah.zafar@nasa.gov>
2026-05-14 15:58:24 -0400

    Add keys for inline GH copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,4 +1,18 @@
 return {
   "github/copilot.vim",
+  keys = {
+    { "<leader>Al", ":Copilot<CR>", desc = "Load Copilot Inline" },
+    {'<leader>At', function()
+      local status = vim.fn['copilot#Enabled']()
+      if status == 1 then
+        vim.cmd('Copilot disable')
+        vim.notify('Inline Copilot Disabled')
+      else
+        vim.cmd('Copilot enable')
+        vim.notify('Inline Copilot Enabled')
+      end
+    end, desc = "Toggle GitHub Inline Copilot" },
+  },
 }
+
 
