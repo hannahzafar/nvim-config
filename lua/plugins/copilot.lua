2a583ffc2cdbf40d7308fd6a70c2f05df6b7bdbe
Hannah Zafar <hannah.zafar@nasa.gov>
2026-06-05 12:42:35 -0400

    Lazy load copilot

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,15 +1,12 @@
 return {
   "zbirenbaum/copilot.lua",
-  -- requires = {
-  --   "copilotlsp-nvim/copilot-lsp", -- (optional) for NES functionality
-  -- },
+  lazy = true,
   cmd = "Copilot",
   keys = {
     { "<leader>c" , "<cmd>Copilot<cr>" , desc = "Copilot AI" },
     { "<leader>tc", "<cmd>Copilot suggestion toggle_auto_trigger<cr>", desc = "[T]oggle [C]opilot Suggestions" },
   },
 
-  event = "InsertEnter",
   config = function()
     require("copilot").setup({
       panel = { enabled = false },
